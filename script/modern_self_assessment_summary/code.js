e2fe39727964c86a5078e355327af45d60ca2529
hyamakawa <h-yamakawa@esm.co.jp>
2022-06-22 16:35:47 +0900

    中間ふりかえり記録機能を追加して、エラー発生時の対応も入れた

--- script/modern_self_assessment_summary/code.js
+++ script/modern_self_assessment_summary/code.js
@@ -20,13 +20,14 @@ function onOpen () {
   SpreadsheetApp
     .getActiveSpreadsheet()
     .addMenu("カスタムメニュー", [
-      {name: "今開いてるシートのモダン自己評価記録を更新する", functionName: "updateSelfAssessmentRecordOperation"}
+      {name: "今開いてるシートのモダン自己評価記録を更新する", functionName: "updateSelfAssessmentRecordOperation"},
+      {name: "今開いてるシートの中間ふりかえり記録を更新する", functionName: "updateMidtermReviewRecordOperation"}
     ])
 }
 
 /**
  * トリガーで実行する関数
- * 全てのチャレンジャーシートの該当日付データに集計したデータを貼り付ける
+ * 全てのチャレンジャーシートの該当日付データにモダン自己評価を貼り付ける
  */
 function updateSelfAssessmentRecordTrigger () {
   try {
@@ -34,6 +35,7 @@ function updateSelfAssessmentRecordTrigger () {
     const challengerData = selectChallengerData_()
 
     // challengerDataを元に各チャレンジャーごとにシートを更新する
+    let errorMessage = ''
     for (challenger of challengerData) {
       // 更新対象シートの諸々のデータを取得する
       const updateSheetData = selectUpdateSheetData_(challenger.name)
@@ -43,24 +45,77 @@ function updateSelfAssessmentRecordTrigger () {
         throw new Error(`チャレンジャーシート以外のシートで処理が実行されました シート名: ${challenger.name}`)
       }
 
-      // 更新する範囲にすでに値が入っていないか事前チェックする
-      const alreadySetValueNum = updateSheetData.thisTimeRange.getValues()[0].reduce((pre, cur) => {
-        cur !== '' && pre++
-        return pre
-      }, 0)
-      if (alreadySetValueNum > 0) {
-        throw new Error(`更新行にすでに何か値が設定されています シート名: ${challenger.name}`)
-      }
-
       // 更新対象シートを更新する
-      updateSelfAssessmentRecord_({ ...challenger, ...updateSheetData })
+      try {
+        updateSelfAssessmentRecord_({ ...challenger, ...updateSheetData })
+      } catch (e) {
+        errorMessage += e + "\n"
+      }
     }
 
     // team_modern_studyチャンネルに完了メッセージを投稿する
-    let message = "モダン自己評価を学習記録に更新したよっ\n"
+    let message = "<!channel> \n"
+    message += "モダン自己評価を学習記録に更新したよっ\n"
     message += "みんな見てみてね :baby_chick:\n"
     message += "\n"
     message += "https://datastudio.google.com/s/rdVaSsI1-8o\n"
+
+    if (errorMessage) {
+      message += "\n"
+      message += "----------\n"
+      message += errorMessage
+    }
+
+    notifySlack_(message)
+  } catch {
+    // team_modern_studyチャンネルにエラーメッセージを投稿する
+    let message = "モダン自己評価を更新しようとしたらエラーが発生したよっ :cry:\n"
+    message += "エラーになったシート名: " + challenger.name + "\n"
+    notifySlack_(message)
+  }
+}
+
+/**
+ * トリガーで実行する関数
+ * 全てのチャレンジャーシートの該当日付データに中間ふりかえりを貼り付ける
+ */
+ function updateMidtermReviewRecordTrigger () {
+  try {
+    // 環境設定シートからデータを取得する
+    const challengerData = selectChallengerData_()
+
+    // challengerDataを元に各チャレンジャーごとにシートを更新する
+    let errorMessage = ''
+    for (challenger of challengerData) {
+      // 更新対象シートの諸々のデータを取得する
+      const updateSheetData = selectUpdateSheetData_(challenger.name)
+
+      // 開いているシートが各チャレンジャーシート以外の場合は処理を終了する
+      if (!challengerData.some(challennger => challennger.name === updateSheetData.name)) {
+        throw new Error(`チャレンジャーシート以外のシートで処理が実行されました シート名: ${challenger.name}`)
+      }
+
+      // 更新対象シートを更新する
+      try {
+        updateMidtermReviewRecord_({ ...challenger, ...updateSheetData })
+      } catch (e) {
+        errorMessage += e + "\n"
+      }
+    }
+
+    // team_modern_studyチャンネルに完了メッセージを投稿する
+    let message = "<!channel> \n"
+    message += "モダン自己評価の中間ふりかえりを学習記録に更新したよっ\n"
+    message += "みんな見てみてね :baby_chick:\n"
+    message += "\n"
+    message += "https://datastudio.google.com/s/rdVaSsI1-8o\n"
+
+    if (errorMessage) {
+      message += "\n"
+      message += "----------\n"
+      message += errorMessage
+    }
+
     notifySlack_(message)
   } catch {
     // team_modern_studyチャンネルにエラーメッセージを投稿する
@@ -72,7 +127,7 @@ function updateSelfAssessmentRecordTrigger () {
 
 /**
  * スプレッドシートを開いて実行する関数
- * 1つのチャレンジャーシートの該当日付データに集計したデータを貼り付ける
+ * 1つのチャレンジャーシートの該当日付データにモダン自己評価を貼り付ける
  */
 function updateSelfAssessmentRecordOperation () {
   // 環境設定シートからデータを取得する
@@ -93,19 +148,53 @@ function updateSelfAssessmentRecordOperation () {
     return
   }
 
-  // 更新する範囲にすでに値が入っていないか事前チェックする
-  const alreadySetValueNum = updateSheetData.thisTimeRange.getValues()[0].reduce((pre, cur) => {
-    cur !== '' && pre++
-    return pre
-  }, 0)
-  if (alreadySetValueNum > 0) {
-    Browser.msgBox('すでに何か値が設定されていそうなので、\\n更新したいならA~N列には値が設定されていない状態にしてください。')
+  // 更新対象シートを更新する
+  const email = challengerData.find(challenger => challenger.name === updateSheetData.name).email
+  try {
+    updateSelfAssessmentRecord_({ email, ...updateSheetData })
+  } catch (e) {
+    Browser.msgBox(e)
+    return
+  }
+}
+
+/**
+ * スプレッドシートを開いて実行する関数
+ * 1つのチャレンジャーシートの該当日付データに中間ふりかえりを貼り付ける
+ */
+function updateMidtermReviewRecordOperation () {
+  // 環境設定シートからデータを取得する
+  const challengerData = selectChallengerData_()
+
+  // 更新対象シートの諸々のデータを取得する
+  const updateSheetData = selectUpdateSheetData_(SpreadsheetApp.getActiveSheet().getName())
+
+  // 開いているシートが各チャレンジャーシート以外の場合は処理を終了する
+  if (!challengerData.some(challenger => challenger.name === updateSheetData.name)) {
+    Browser.msgBox('このシートでは実行できません。\\n各チャレンジャーシートで実行してください。')
+    return
+  }
+
+  // モダン自己評価を実行する前に実施した場合は処理を終了する
+  if (!updateSheetData.thisTimeRange.getValues()[0][0]) {
+    Browser.msgBox('まだ中間ふりかえりの反映はできません。\\n先にモダン自己評価を反映後、実行してください。')
+    return
+  }
+
+  // 更新対象の確認ダイアログを表示する
+  const updateRow = updateSheetData.thisTimeRange.getRow()
+  if (Browser.msgBox(`${updateRow} 行目のデータを更新しますが、更新対象合ってますか？`, Browser.Buttons.OK_CANCEL) === 'cancel') {
     return
   }
 
   // 更新対象シートを更新する
   const email = challengerData.find(challenger => challenger.name === updateSheetData.name).email
-  updateSelfAssessmentRecord_({ email, ...updateSheetData })
+  try {
+    updateMidtermReviewRecord_({ email, ...updateSheetData })
+  } catch (e) {
+    Browser.msgBox(e)
+    return
+  }
 }
 
 /**
@@ -136,15 +225,30 @@ function selectUpdateSheetData_ (name) {
   if (!(mySheetValues.length > 1 || mySheetValues[0].length > 1)) {
     return {
       name: mySheet.getName(),
-      FirstTimeRange: null,
-      thisTimeRange: null
+      thisTimeRange: null,
+      learnEagernessReasonRange: null,
+      openReasonRange: null,
+      competenceReasonRange: null,
+      subjectiveReasonRange: null,
+      harmonyReasonRange: null,
+      strengthenElementRange: null,
+      strengthenActionRange: null,
+      thisTimeRange: null,
+      learnEagernessReasonRange: null,
+      openReasonRange: null,
+      competenceReasonRange: null,
+      subjectiveReasonRange: null,
+      harmonyReasonRange: null,
+      strengthenElementRange: null,
+      strengthenActionRange: null,
+      midtermReviewRange: null
     }
   }
 
   // 更新する学習記録範囲を特定する
   const updateRowNum = 1
   const updateRowFirstTime = 2
-  const updateRowThisTime = mySheet.getLastRow() + 1
+  const updateRowThisTime = calcUpdateRowThisTime_(mySheet)
   const updateColumn = 1 //年月から
   const updateNumColumn = mySheetValues[0].length // 中間ふりかえりまで
 
@@ -157,21 +261,35 @@ function selectUpdateSheetData_ (name) {
     subjectiveReasonRange: mySheet.getRange(updateRowFirstTime, 9),
     harmonyReasonRange: mySheet.getRange(updateRowFirstTime, 11),
     strengthenElementRange: mySheet.getRange(updateRowFirstTime, 12),
-    strengthenActionRange: mySheet.getRange(updateRowFirstTime, 13)
+    strengthenActionRange: mySheet.getRange(updateRowFirstTime, 13),
+    midtermReviewRange: mySheet.getRange(updateRowFirstTime, 14)
   }
 }
 
+function calcUpdateRowThisTime_ (mySheet) {
+  const today = new Date()
+  const lastRow = mySheet.getLastRow()
+  const lastRowDate = mySheet.getRange(lastRow, 1).getValue()
+  return (lastRow > 1 && lastRowDate && lastRowDate.getFullYear() === today.getFullYear() && lastRowDate.getMonth() === today.getMonth())
+    ? lastRow
+    : lastRow + 1
+}
+
 /**
- * 更新対象シートを更新する
+ * 更新対象シートを更新する（モダン自己評価）
  */
-function updateSelfAssessmentRecord_({ email, thisTimeRange, learnEagernessReasonRange, openReasonRange, competenceReasonRange, subjectiveReasonRange, harmonyReasonRange, strengthenElementRange, strengthenActionRange }) {
+function updateSelfAssessmentRecord_ ({ name, email, thisTimeRange, learnEagernessReasonRange, openReasonRange, competenceReasonRange, subjectiveReasonRange, harmonyReasonRange, strengthenElementRange, strengthenActionRange }) {
   const today = new Date()
   const searchStartDate = new Date(today.getFullYear(), today.getMonth(), 1, 0, 0, 0)
   const searchEndDate = new Date(today.getFullYear(), today.getMonth() + 1, 0, 23, 59, 59)
 
   // 各種シートからチャレンジャー名に紐づくデータを取得する
   const selfAssessmentData = selectSelfAssessmentData_(email, searchStartDate, searchEndDate)
-  // const midtermReviewData = selectMidtermReviewData_(email, searchStartDate, searchEndDate)
+
+  // 該当データがない場合はエラーをスローする
+  if (!selfAssessmentData) {
+    throw new Error(`${name} さんは回答中です`)
+  }
 
   // 更新データを作成する
   const updateThisTimeValues =  thisTimeRange.getValues()
@@ -202,6 +320,26 @@ function updateSelfAssessmentRecord_({ email, thisTimeRange, learnEagernessReaso
 }
 
 /**
+ * 更新対象シートを更新する（中間ふりかえり）
+ */
+ function updateMidtermReviewRecord_ ({ name, email, midtermReviewRange }) {
+  const today = new Date()
+  const searchStartDate = new Date(today.getFullYear(), today.getMonth(), 1, 0, 0, 0)
+  const searchEndDate = new Date(today.getFullYear(), today.getMonth() + 1, 0, 23, 59, 59)
+
+  // 各種シートからチャレンジャー名に紐づくデータを取得する
+  const midtermReviewData = selectMidtermReviewData_(email, searchStartDate, searchEndDate)
+   
+  // 該当データがない場合はエラーをスローする
+  if (!midtermReviewData) {
+    throw new Error(`${name} さんは回答中です`)
+  }
+   
+  // 更新データをスプレッドシートに反映する
+  midtermReviewRange.setValue(midtermReviewData.midtermReview) // 中間ふりかえり
+}
+
+/**
  *モダン自己評価シートから指定されたチャレンジャー名、日付に紐づくデータを取得する
  */
 function selectSelfAssessmentData_ (myEmail, searchStartDate, searchEndDate) {
@@ -210,31 +348,40 @@ function selectSelfAssessmentData_ (myEmail, searchStartDate, searchEndDate) {
     .filter(row => row[1] === myEmail)
     .filter(row => searchStartDate.getTime() <= row[0].getTime() && row[0].getTime() <= searchEndDate.getTime())
   const latestIndex = myData.length - 1
-  return {
-    learnEagernessPoint: myData[latestIndex][2], // 学習意欲評価
-    learnEagernessReason: myData[latestIndex][3], // 学習意欲評価理由
-    openPoint: myData[latestIndex][4], // オープン評価
-    openReason: myData[latestIndex][5], // オープン評価理由
-    competencePoint: myData[latestIndex][6], // 言語力評価
-    competenceReason: myData[latestIndex][7], // 言語力評価理由
-    subjectivePoint: myData[latestIndex][8], // 主体性評価
-    subjectiveReason: myData[latestIndex][9], // 主体性評価理由
-    harmonyPoint: myData[latestIndex][10], // 協調性評価
-    harmonyReason: myData[latestIndex][11], // 協調性評価理由
-    strengthenElement: myData[latestIndex][12], // 改善・強化マインド要素
-    strengthenAction: myData[latestIndex][13] // 改善・強化tryAction
-  }
+
+  return latestIndex === -1
+    ? null // アンケート回答がなかったときはnullを返す
+    : {
+      learnEagernessPoint: myData[latestIndex][2], // 学習意欲評価
+      learnEagernessReason: myData[latestIndex][3], // 学習意欲評価理由
+      openPoint: myData[latestIndex][4], // オープン評価
+      openReason: myData[latestIndex][5], // オープン評価理由
+      competencePoint: myData[latestIndex][6], // 言語力評価
+      competenceReason: myData[latestIndex][7], // 言語力評価理由
+      subjectivePoint: myData[latestIndex][8], // 主体性評価
+      subjectiveReason: myData[latestIndex][9], // 主体性評価理由
+      harmonyPoint: myData[latestIndex][10], // 協調性評価
+      harmonyReason: myData[latestIndex][11], // 協調性評価理由
+      strengthenElement: myData[latestIndex][12], // 改善・強化マインド要素
+      strengthenAction: myData[latestIndex][13] // 改善・強化tryAction
+    }
 }
 
 /**
  * 中間ふりかえりシートから指定されたチャレンジャー名、日付に紐づくデータを取得する
  */
-function selectMidtermReviewData_ (myEmail) {
-  const originalSheetValues = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(REFERENCE_SHEET.MIDTERM_REVIEW).getDataRange().getValues()
-  const myColumnIndex = originalSheetValues[0].findIndex(column => column === myEmail)
-  return {
-    studyAutonomy: originalSheetValues[1][myColumnIndex]
-  }
+function selectMidtermReviewData_ (myEmail, searchStartDate, searchEndDate) {
+  const midtermReviewSheetValues = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(REFERENCE_SHEET.MIDTERM_REVIEW).getDataRange().getValues()
+  const myData = midtermReviewSheetValues
+    .filter(row => row[1] === myEmail)
+    .filter(row => searchStartDate.getTime() <= row[0].getTime() && row[0].getTime() <= searchEndDate.getTime())
+  const latestIndex = myData.length - 1
+
+  return latestIndex === -1
+    ? null // アンケート回答がなかったときはnullを返す
+    : {
+      midtermReview: myData[latestIndex][2], //中間ふりかえり
+    }
 }
 
 /**
